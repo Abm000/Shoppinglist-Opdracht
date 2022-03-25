0ec84479a468b5275e4ae4ed50108af9e38bbb01
abm12 <andreh.almoussa@hva.nl>
2022-03-25 10:17:27 +0100

    Standard user to login

---
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,31 @@
+package com.example.backend;
+
+import com.example.backend.models.Product;
+import com.example.backend.models.User;
+import com.example.backend.repository.ProductServ;
+import com.example.backend.repository.UserServ;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication implements CommandLineRunner {
+    @Autowired
+    private UserServ userServ;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
+
+        User user = new User();
+        user.setPassword("30");
+        user.setUserName("John");
+        userServ.save(user);
+
+
+
+    }
+}
